2b3dea1feeed6e0391ec0e760e3376f9353b39de
Phalakorn223 <phalakorn061@gmail.com>
2022-01-24 10:58:47 +0700

    update

--- lib/pages/row_column_demo.dart
+++ lib/pages/row_column_demo.dart
@@ -1,30 +1,67 @@
 import 'package:flutter/material.dart';
 
 class RowColumnDemo extends StatelessWidget {
-  const RowColumnDemo({Key? key,}) : super(key: key);
+  const RowColumnDemo({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter App"),
+        title: Text('Flutter App'),
         centerTitle: true,
       ),
-      body: Container(
-        height: 80,
-        color: Colors.blue,
-        child: Row(
-          children: [
-            Column(
-              children: [
-                Icon(Icons.save,color: Colors.white),
-                Text('Prep',style: TextStyle(color: Colors.white),),
-                Text('25 min',style: TextStyle(color: Colors.white),)
-              ],
-            ),
-            Column(),
-            Column(),
-          ],
+      body: Center(
+        child: Container(
+          height: 100,
+          color: Colors.blue,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.account_balance,color: Colors.white),
+                  SizedBox(height: 6,),
+                  Text('Cats',style: TextStyle(color: Colors.white),),
+                  Text('Version Korat',style: TextStyle(color: Colors.white),),
+                ],
+              ),
+              SizedBox(width: 12,),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.alarm,color: Colors.white),
+                  SizedBox(height: 6,),
+                  Text('Flutter',style: TextStyle(color: Colors.white),),
+                  Text('Version 2.0',style: TextStyle(color: Colors.white),),
+                ],
+              ),
+              SizedBox(width: 12,),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.accessibility,color: Colors.white),
+                  SizedBox(height: 6,),
+                  Text('React N',style: TextStyle(color: Colors.white),),
+                  Text('5.0',style: TextStyle(color: Colors.white),),
+                ],
+              ),
+              SizedBox(width: 12,),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.accessibility,color: Colors.white),
+                  SizedBox(height: 6,),
+                  Text('React N',style: TextStyle(color: Colors.white),),
+                  Text('5.0',style: TextStyle(color: Colors.white),),
+                ],
+              ),
+              SizedBox(width: 12,),
+            ],
+          ),
         ),
       ),
     );
